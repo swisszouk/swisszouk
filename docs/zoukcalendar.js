2c70e07342f635d58574a0dd37b9da57efc2128f
Wojciech Ptak <w@ptak.me>
2022-10-10 18:12:38 +0200

    hide past events and separators

---
+++ docs/zoukcalendar.js
@@ -0,0 +1,19 @@
+var zoukcalendar = (() => {
+    const redraw = () => {
+        const now = (new Date()).toISOString().slice(0, 10)
+        document.querySelectorAll('.event').forEach(el => {
+            const evDate = el.getAttribute('data-date')
+            if (evDate < now) {
+                el.style.display = 'none'
+            }
+        })
+    }
+
+    if (document.readyState == 'loading') {
+        document.addEventListener('DOMContentLoaded', redraw)
+    } else {
+        redraw()
+    }
+
+    return { redraw }
+})()
