a5085c49002d118ec4f10e1959c1d4dd25c67b5b
Wojciech Ptak <w@ptak.me>
2022-10-10 21:06:36 +0200

    fix dates

--- docs/zoukcalendar.js
+++ docs/zoukcalendar.js
@@ -1,13 +1,18 @@
 var zoukcalendar = (() => {
     const redraw = () => {
+        const n = new Date()
         const now = (new Date()).toISOString().slice(0, 10)
+        let timeout = new Date()
+        timeout.setDate(timeout.getDate() + 30)
+        timeout = timeout.toISOString().slice(0, 10)
+        console.log(`timeout: ${timeout}`)
         document.querySelectorAll('.event').forEach(el => {
             const evDate = el.getAttribute('data-date')
             if (evDate < now) {
                 el.style.display = 'none'
             }
         })
-        document.getElementById('content-container').style.visibility = 'visible'
+        document.getElementById('content-container').style.display = 'block'
     }
 
     if (document.readyState == 'loading') {
